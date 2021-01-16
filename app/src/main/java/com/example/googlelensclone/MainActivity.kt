67ab3031b651cd974a0e7fee3e7a55256faee4d9
vmehra25 <vmehra25@yahoo.com>
2021-01-16 16:07:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/googlelensclone/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.googlelensclone
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        @JvmStatic val REQUEST_IMAGE_CAPTURE = 1
+        @JvmStatic val EXTRA_DATA = "data"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnTakePhoto.setOnClickListener {
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE)
+            }catch (e: Exception){
+                Log.d("ERR_MSG", "Error $e")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE){
+            val bitmap = data?.extras?.get(EXTRA_DATA) as Bitmap
+            imgThumb.setImageBitmap(bitmap)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
